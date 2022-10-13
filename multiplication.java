0a431f1317b5697ebcb848ba185a0190de0bd4df
Pranav Tyagi <pranav233tyagi@gmail.com>
2022-10-13 18:23:28 +0530

    added multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class multiplication
+{
+    public static void main(String args[])
+    {
+        int a =20;int b= 10;
+        System.out.println("multiplication of two numbers is " + (a*b));
+    }
+
+}
